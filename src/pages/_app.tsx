898fee63fd1c1b5a0aae54966b78462f5af9b4f5
edsattar <ahmed.sattar@live.com>
2023-04-30 05:01:18 +0600

    Added auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,32 @@
+import { ClerkProvider, SignedIn } from "@clerk/nextjs";
 import { type AppType } from "next/app";
+import { useRouter } from "next/router";
 
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
 
+const publicPages = ["/sign-in/[[...index]]", "/sign-up/[[...index]]"];
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  // Get the pathname
+  const { pathname } = useRouter();
+
+  // Check if the current route matches a public page
+  const isPublicPage = publicPages.includes(pathname);
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+      {/* {isPublicPage ? (
+      ) : (
+        <>
+          <SignedIn>
+            <Component {...pageProps} />
+          </SignedIn>
+        </>
+      )} */}
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
